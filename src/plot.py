72e0f0044eaf76f58f0977f9747018a1e048199a
Tom Logan <tomlogan@umich.edu>
2018-10-09 19:19:18 -0400

    plotting in geopandas

--- src/plot.py
+++ src/plot.py
@@ -9,5 +9,21 @@ cursor = con.cursor()
 sql = "SELECT block.geoid10, block.geom FROM block, city WHERE ST_Intersects(block.geom, ST_Transform(city.geom, 4269)) AND city.juris = 'WM'"
 df = gpd.GeoDataFrame.from_postgis(sql, con, geom_col='geom')
 
-df.plot()
+sql = "SELECT id, dest_type, geom FROM destinations"
+dests = gpd.GeoDataFrame.from_postgis(sql, con)
+
+
+# plot
+fig, ax = plt.subplots()
+
+ax.set_aspect('equal')
+
+# plot blocks
+df.plot(ax=ax, color='white', edgecolor='black')
+# plot super markets
+dests[dests['dest_type']=='super_market'].plot(ax=ax, marker='s', color='blue', markersize=5);
+dests[dests['dest_type']=='gas_station'].plot(ax=ax, marker='o', color='red', markersize=5);
+
+
+# df.plot()
 plt.show()
